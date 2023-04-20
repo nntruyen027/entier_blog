45f420ae58a672856f25f8739f55d701b5a238d9
Nguyễn Ngọc Truyện <truyen027@gmail.com>
2023-04-20 19:12:15 +0700

    date 3 - ver 1

---
+++ assets/js/home.js
@@ -0,0 +1,12 @@
+let join_button
+
+document.addEventListener("DOMContentLoaded", function () {
+    join_button = document.getElementsByClassName("banner-contact-button")[0];
+
+
+});
+
+console.log(join_button)
+
+
+
