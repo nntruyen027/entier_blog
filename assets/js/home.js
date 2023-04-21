c3e693ff1ebe36532069a48169adf50f9b71d82a
Nguyễn Ngọc Truyện <truyen027@gmail.com>
2023-04-21 14:26:16 +0700

    date 4 - ver 1

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,12 +1,30 @@
-let join_button
-
 document.addEventListener("DOMContentLoaded", function () {
-    join_button = document.getElementsByClassName("banner-contact-button")[0];
+    let join_button, contact_background, contact_container, contact_close
+
+    join_button = document.getElementsByClassName("banner-contact-button")[0]
+    contact_background = document.getElementsByClassName("contact-background")[0]
+    contact_container = document.getElementsByClassName("contact-container")[0]
+    contact_close = document.querySelector('.contact>.close');
+    contact_submit = document.querySelector('.contact>.contact-submit>input')
 
 
+    join_button.addEventListener("click", function () {
+        contact_background.style.display = "block"
+        contact_container.style.display = "block"
+    })
+
+    contact_close.addEventListener("click", function () {
+        contact_background.style.display = "none"
+        contact_container.style.display = "none"
+    })
+
+    contact_submit.addEventListener("click", function () {
+        contact_background.style.display = "none"
+        contact_container.style.display = "none"
+    })
 });
 
-console.log(join_button)
+
 
 
 
